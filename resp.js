b83155030671b5695decd2f473bc38f28a53c952
govind <govindkumar952365@gmail.com>
2024-04-28 11:50:42 +0530

    changes

--- resp.js
+++ resp.js
@@ -2,8 +2,12 @@ burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 rightnav = document.querySelector('.rightnav')
 navlist = document.querySelector('.nav-list')
-burger.addEventlistener('click',()=>{
+
+
+
+
+burger.addEventlistener('click', () => {
     rightnav.classlist.toggle('v-class-resp');
-    nav-list.classlist.toggle('v-class-resp');
+    nav - list.classlist.toggle('v-class-resp');
     navbar.classlist.toggle('h-nav-resp');
 })
